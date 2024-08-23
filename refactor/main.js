06f998b443a088528b0e2ec9ec05a7140009ab0b
Fcaspirro <fabio_caspirro@hotmail.com>
2024-08-22 22:10:09 -0300

    refactor: update in header and footer nav and your running

--- refactor/main.js
+++ refactor/main.js
@@ -1,3 +1,4 @@
+// Config cursor
 const { gsap } = window;
 
 const cursorOuter = document.querySelector(".cursor--large");
@@ -49,6 +50,7 @@ $(document).ready(function() {
 $('.nav-btn').on('click', function(event) {
   event.preventDefault();
   let targetId = $(this).data('target');
+  
   $('.content-nav li a').removeClass('active-nav');
   $('section').removeClass('active-section');
   $(this).addClass('active-nav');  
@@ -56,11 +58,71 @@ $('.nav-btn').on('click', function(event) {
 });
 
 // Footer nav
-$('.footer-nav-btn').on('click', function(event) {
+$('.footer-nav-btn').not('.downloader').on('click', function(event) {
+  let $this = $(this); 
+
+  if ($this.hasClass('external-link')) {
+    $this.addClass('active-footer-nav');
+    setTimeout(function() {
+      $this.removeClass('active-footer-nav');
+    }, 2000);
+
+    return;
+  }
+  
+  event.preventDefault();
+  $this.addClass('active-footer-nav');
+});
+
+
+// Footer nav downloader
+$('.footer-nav-btn.downloader').on('click', function(event) {
   event.preventDefault();
   let $this = $(this);
-  $this.addClass('active-footer-nav');
-  setTimeout(function() {
-    $this.removeClass('active-footer-nav');
-  }, 5000);
+  let $wrapperOptions = $this.find('.wrapper-options');
+  
+  if ($this.hasClass('active-downloader')) {
+    gsap.to($wrapperOptions, {
+      opacity: 0,
+      y: -20,
+      duration: 0.3,
+      onComplete: function() {
+        $this.removeClass('active-footer-nav active-downloader');
+        $wrapperOptions.css('display', 'none').attr('aria-hidden', 'true');
+      }
+    });
+  } else {
+    $('.footer-nav-btn.downloader').removeClass('active-footer-nav active-downloader');
+    $('.wrapper-options').each(function() {
+      gsap.to($(this), {
+        opacity: 0,
+        y: -20,
+        duration: 0.3,
+        onComplete: function() {
+          $(this).css('display', 'none').attr('aria-hidden', 'true');
+        }
+      });
+    });
+    $this.addClass('active-footer-nav active-downloader');
+    $wrapperOptions.css('display', 'flex').attr('aria-hidden', 'false');
+    gsap.fromTo($wrapperOptions, {
+      opacity: 0,
+      y: -20
+    }, {
+      opacity: 1,
+      y: 0,
+      duration: 0.3
+    });
+  }
+});
+
+// Download
+$('.wrapper-options button').on('click', function() {
+  let fileUrl = $(this).data('url');
+
+  if (fileUrl) {
+    window.open(fileUrl, '_blank');
+  } else {
+    console.error('Arquivo não encontrado!');
+  }
 });
